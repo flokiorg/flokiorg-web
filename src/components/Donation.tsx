4e23426d2c08668fa17d00abc1efa76d87b7d7d7
devxaro <217111225+devxaro@users.noreply.github.com>
2025-08-31 00:01:12 +0200

    change donation widget

--- src/components/Donation.tsx
+++ src/components/Donation.tsx
@@ -1,19 +1,10 @@
 import React from "react";
 import styles from "@styles/scss/Donations.module.scss";
 import { useTranslation } from "react-i18next";
-import { DONATION_APIKEY } from "@constants/config";
+import { DONATION_URL } from "@constants/config";
 
 const Donation = () => {
   const { t } = useTranslation();
-  const widgetUrl = `https://nowpayments.io/embeds/donation-widget?api_key=${DONATION_APIKEY}`;
-
-  // Style objet, typé correctement
-  const iframeStyle: React.CSSProperties = {
-    overflowY: "hidden",
-    borderRadius: 11,
-    border: "2px solid #d0970f", // gold
-    boxShadow: "0 10px 30px rgba(0,0,0,.45)",
-  };
 
   return (
     <section className={styles.donationSection} id="donation">
@@ -27,19 +18,24 @@ const Donation = () => {
             </div>
           </div>
           <p className={styles.description}>{t("donation.description")}</p>
+          <a
+            href={DONATION_URL}
+            target="_blank"
+            rel="noopener noreferrer"
+            className={styles.sectionButton}
+          >
+            <img
+              src="/assets/heart.png"
+              alt="Heart Icon"
+              className={styles.icon}
+            />
+            {t("donation.buttonLabel")}
+          </a>
         </div>
 
         <div className={styles.rightColumn}>
-          <div className={styles.widgetWrapper}>
-            <iframe
-              title="Crypto Donation Widget"
-              src={widgetUrl}
-              width={346}
-              height={570}
-              frameBorder={0}
-              scrolling="no"
-              style={iframeStyle}
-            />
+          <div className={styles.imageWrapper}>
+            <img src="/img/wof.png" className={styles.image} />
           </div>
         </div>
       </div>
